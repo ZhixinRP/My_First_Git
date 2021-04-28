91f56e8deff75eb274894790862fc23a9ca14e4c
liu_zhixin <liu_zhixin@connect.ite.edu.sg>
2021-04-28 10:26:16 +0800

    Added comment

--- app/src/main/java/sg/edu/rp/c346/id20047401/demomysecondapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20047401/demomysecondapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //THIS IS A COMMENT
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
