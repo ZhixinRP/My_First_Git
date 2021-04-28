125a8cb37762a8dae034d82ced59e0980659d57a
liu_zhixin <liu_zhixin@connect.ite.edu.sg>
2021-04-28 15:29:36 +0800

    Added a more projects

---
+++ app/src/main/java/sg/edu/rp/c346/id20047401/demomysecondapp/MainActivity2c.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20047401.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2c extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2c);
+    }
+}
